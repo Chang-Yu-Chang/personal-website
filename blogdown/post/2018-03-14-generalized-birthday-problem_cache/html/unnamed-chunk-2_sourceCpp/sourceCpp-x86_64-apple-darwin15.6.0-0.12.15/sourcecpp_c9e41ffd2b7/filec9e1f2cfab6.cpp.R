67455810c5f985cc916a30f91392fd4c780ad833
ray57512 <ray57512@gmail.com>
2018-03-16 15:59:23 -0400

    generalized bithday problem

---
+++ blogdown/post/2018-03-14-generalized-birthday-problem_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-apple-darwin15.6.0-0.12.15/sourcecpp_c9e41ffd2b7/filec9e1f2cfab6.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/Users/cychang/Desktop/Personal-Website/content/post/2018-03-14-generalized-birthday-problem_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-apple-darwin15.6.0-0.12.15/sourcecpp_c9e41ffd2b7/sourceCpp_2.so')
+
+fibonacci <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_fibonacci')
+
+rm(`.sourceCpp_1_DLLInfo`)
