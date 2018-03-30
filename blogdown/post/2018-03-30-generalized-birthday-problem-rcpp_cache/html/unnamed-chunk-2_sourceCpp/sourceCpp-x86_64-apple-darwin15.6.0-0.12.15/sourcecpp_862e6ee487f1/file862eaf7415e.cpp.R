af04cce70eccc1ebb12b59795301429ccdeb6c2c
ray57512 <ray57512@gmail.com>
2018-03-30 12:26:48 -0400

    Rcpp

---
+++ blogdown/post/2018-03-30-generalized-birthday-problem-rcpp_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-apple-darwin15.6.0-0.12.15/sourcecpp_862e6ee487f1/file862eaf7415e.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/Users/cychang/Desktop/Personal-Website/content/post/2018-03-30-generalized-birthday-problem-rcpp_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-apple-darwin15.6.0-0.12.15/sourcecpp_862e6ee487f1/sourceCpp_2.so')
+
+simulateC <- Rcpp:::sourceCppFunction(function(k, room, replicate) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_simulateC')
+
+rm(`.sourceCpp_1_DLLInfo`)
